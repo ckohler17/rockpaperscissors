cd4d0fc1d052babd5bc2750765964ac05079a3de
Carolynn Kohler <ckohler@gogsf.com>
2019-10-23 11:23:11 -0500

    added my classes that I think I will need

---
+++ RockPaperScissors/Computer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Computer : Player
+    {
+        //member variables(Has A)
+
+        //constructor(Builder)
+
+        //member methods(Can Do)
+
+    }
+}
